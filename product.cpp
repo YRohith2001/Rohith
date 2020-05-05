3a5c3cb96cda7baad49f775804bf61107c145ee4
YRohith2001 <63359339+YRohith2001@users.noreply.github.com>
2020-05-05 13:56:37 +0530

    Create product.cpp
    product of two numbers

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+double x,y;
+cout<<"input x\n";
+cin>>x;
+cout<<"input y\n";
+cin>>y;
+cout<<"product of "<<x<<" and "<<y<<" is "<<x*y<<endl;
+return 0;
+}
